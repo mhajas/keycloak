7f9824bf6c34fa94955063c0a512e6e0dd2579e2
Michal Hajas <mhajas@redhat.com>
2021-01-22 09:20:31 +0100

    KEYCLOAK-831 Add test for encrypted assertion

--- testsuite/integration-arquillian/util/src/main/java/org/keycloak/testsuite/utils/io/IOUtil.java
+++ testsuite/integration-arquillian/util/src/main/java/org/keycloak/testsuite/utils/io/IOUtil.java
@@ -28,12 +28,16 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -103,6 +107,19 @@ public class IOUtil {
         }
     }
 
+    public static InputStream documentToInputStream(Document doc) {
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            Source xmlSource = new DOMSource(doc);
+            Result outputTarget = new StreamResult(outputStream);
+            TransformerFactory.newInstance().newTransformer().transform(xmlSource, outputTarget);
+            return new ByteArrayInputStream(outputStream.toByteArray());
+        } catch (TransformerException e) {
+            log.error("Can't transform document to InputStream");
+            throw new RuntimeException(e);
+        }
+    }
+
     /**
      * Modifies attribute value according to the given regex (first occurrence) iff 
      * there are following conditions accomplished:
@@ -119,7 +136,8 @@ public class IOUtil {
      * @param regex
      * @param replacement
      */
-    public static void modifyDocElementAttribute(Document doc, String tagName, String attributeName, String regex, String replacement) {
+    public static void
+    modifyDocElementAttribute(Document doc, String tagName, String attributeName, String regex, String replacement) {
         NodeList nodes = doc.getElementsByTagName(tagName);
         if (nodes.getLength() != 1) {
             log.warn("Not able or ambiguous to find element: " + tagName);
