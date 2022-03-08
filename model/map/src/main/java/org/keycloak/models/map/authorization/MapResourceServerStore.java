c815b04ece9d6f57240fd6354f70435aaab78f5f
Michal Hajas <mhajas@redhat.com>
2022-03-08 15:59:35 +0100

    Replace resourceServerId with resourceServer model in PolicyStore

--- model/map/src/main/java/org/keycloak/models/map/authorization/MapResourceServerStore.java
+++ model/map/src/main/java/org/keycloak/models/map/authorization/MapResourceServerStore.java
@@ -105,7 +105,7 @@ public class MapResourceServerStore implements ResourceServerStore {
 
         // TODO: Simplify the following, ideally by leveraging triggers, stored procedures or ref integrity
         PolicyStore policyStore = authorizationProvider.getStoreFactory().getPolicyStore();
-        policyStore.findByResourceServer(id).stream()
+        policyStore.findByResourceServer(resourceServer).stream()
             .map(Policy::getId)
             .forEach(policyStore::delete);
 
