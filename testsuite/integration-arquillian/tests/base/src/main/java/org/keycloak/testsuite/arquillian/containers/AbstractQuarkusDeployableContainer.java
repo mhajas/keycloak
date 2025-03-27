b37858ed7c0a0d1a69decf19c8f92612963bed35
Michal Hajas <mhajas@redhat.com>
2025-03-27 12:55:17 +0100

    add trace for JDBC ping
    Signed-off-by: Michal Hajas <mhajas@redhat.com>

--- testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/containers/AbstractQuarkusDeployableContainer.java
+++ testsuite/integration-arquillian/tests/base/src/main/java/org/keycloak/testsuite/arquillian/containers/AbstractQuarkusDeployableContainer.java
@@ -208,7 +208,7 @@ public abstract class AbstractQuarkusDeployableContainer implements DeployableCo
         } else {
             commands.add("--cache=ispn");
             commands.add("--cache-config-file=cluster-" + cacheMode + ".xml");
-            commands.add("--log-level=org.keycloak.cluster.infinispan:trace");
+            commands.add("--log-level=org.jgroups.protocols.JDBC_PING2:trace");
 
             var stack = System.getProperty("auth.server.quarkus.cluster.stack");
             if (stack != null)
